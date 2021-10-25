5507df857e4a796da2e0666ba03c40b0ad1e13d0
Juan <panchitoberrio@gmail.com>
2021-10-25 13:54:19 +0200

    Primer Commit

---
+++ src/main/java/com/everis/EverisGit1JuanBerrioRoldan/App.java
@@ -0,0 +1,9 @@
+package com.everis.EverisGit1JuanBerrioRoldan;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "¡Hola mundo! ¡Soy un nuevo Proyecto!" );
+    }
+}
